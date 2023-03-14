5737e53be7598573b14c001c84b041ab08545454
jokull <joj38@hi.is>
2023-03-14 10:55:29 +0000

    asldkjalæd

---
+++ src/main/java/is/hi/hbv202g/ass9/compositeLeafObservedByComposite/MultiplyComposite.java
@@ -0,0 +1,27 @@
+package is.hi.hbv202g.ass9.compositeLeafObservedByComposite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MultiplyComposite implements MathExpressions {
+
+    private List<MathExpressions> comps=new ArrayList<MathExpressions>();
+
+
+    public void add(MathExpressions l){
+        comps.add(l);
+    }
+
+    public void remove(MathExpressions l){
+        comps.remove(l);
+    }
+
+    public int getResult(){
+        int a=1;
+        for (MathExpressions leaf:comps) {
+            a*=leaf.getResult();
+        }
+
+        return a;
+    }
+}
